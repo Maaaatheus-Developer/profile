a32b15f3803694fb3d430264e3fab13a06c4a20d
Matheus Minetto <matheusminetto140502@gmail.com>
2025-02-20 14:17:42 -0300

    styles: novos estilos adicionados

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import { Social } from "../../components/Social/index";
-import { useState, useEffect, use } from "react";
-import { FaFacebook, FaInstagram, FaGithub } from "react-icons/fa";
+import { useState, useEffect } from "react";
+import { FaInstagram, FaGithub, FaWhatsapp } from "react-icons/fa";
 import { db } from "../../services/firebaseConnetion";
 import {
   getDocs,
@@ -74,10 +74,12 @@ export function Home() {
 
   return (
     <div className="flex flex-col w-full py-4 items-center justify-center">
-      <h1 className="md:text-4xl text-3x1 font-bold text-white mt-20">
+      <h1 className="md:text-4xl text-3x1 font-bold text-white mt-30 mb-5">
         Matheus Minetto
       </h1>
-      <span className="text-gray-50 mb-5 mt-3">Veja meus links</span>
+      <span className="text-gray-50 mb-5 mt-3 animate-bounce">
+        Veja meus links 👇🏻
+      </span>
       <main className="flex flex-col w-11/12 max-w-xl text-center">
         {links.map((link) => (
           <section
@@ -94,17 +96,32 @@ export function Home() {
         ))}
 
         {socialLinks && Object.keys(socialLinks).length > 0 && (
-          <footer className="flex justify-center gap-3 my-4">
+          <footer className="flex justify-center gap-3 my-4 ">
             <Social url={socialLinks?.facebook}>
-              <FaFacebook size={35} color="#FFF" />
+              <FaWhatsapp
+                className="select-none transition-transform hover:scale-105 cursor-pointer animate-bounce"
+                size={35}
+                color="#FFF"
+              />
             </Social>
 
             <Social url={socialLinks?.instagram}>
-              <FaInstagram size={35} color="#FFF" />
+              <FaInstagram
+                className="select-none transition-transform hover:scale-105 cursor-pointer animate-bounce
+"
+                size={35}
+                color="#FFF"
+              />
             </Social>
 
             <Social url={socialLinks?.github}>
-              <FaGithub size={35} color="#FFF" />
+              <FaGithub
+                className="select-none transition-transform hover:scale-105 cursor-pointer animate-bounce
+
+"
+                size={35}
+                color="#FFF"
+              />
             </Social>
           </footer>
         )}
